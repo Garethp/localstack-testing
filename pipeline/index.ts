750f038449e629b4f303b6a6cf80c3ec65d13df4
Gareth Parker <blackfireweb@gmail.com>
2024-03-27 12:13:17 +0000

    Show the issue with DNS Alias looping

--- pipeline/index.ts
+++ pipeline/index.ts
@@ -1,3 +1,10 @@
 import { App } from "aws-cdk-lib";
+import { DnsAliasStack } from "./DnsAliasStack";
 
 const app = new App();
+new DnsAliasStack(app, "dns-alias-stack", {
+  env: {
+    account: "000000000000",
+    region: "eu-west-2",
+  },
+});
--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+CLOUDFRONT_URL=$(awslocal cloudfront list-distributions | jq -r .DistributionList.Items[0].DomainName)
+curl "https://$CLOUDFRONT_URL/"
