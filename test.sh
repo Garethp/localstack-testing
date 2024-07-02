1fdd895ca1fa597a3355e59c8eb65487563191b0
Gareth Parker <blackfireweb@gmail.com>
2024-07-02 12:14:48 +0100

    Simplify test command

--- test.sh
+++ test.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-output=$(curl "https://example.localhost.localstack.cloud/?test=test")
-
-echo $output
-
-if [ "$output" == "null" ]; then
-  echo "Expected queryStringParameters of test=test to be passed back. Received nothing"
-fi
+curl "https://example.localhost.localstack.cloud/?test=test"
