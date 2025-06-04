b79a1ee8351b4ce90990741289abfea86c0118e4
Gareth Parker <gareth.parker@ros.gov.uk>
2025-06-04 18:04:36 +0100

    Update the start script for CI

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+LOCALSTACK_VERSION=$(cat docker-compose.yml | sed -nE 's/image: localstack\/localstack-pro:([0-9.]+)/\1/p' | xargs)
+
 docker-compose down
+docker pull localstack/localstack-pro:$LOCALSTACK_VERSION
 docker-compose up -d
 
-while [ ! "$(curl -s http://localhost:4566/_localstack/health | jq -c '.services | .[] | test("running|available") | not' | grep "false")" ]; do
-  sleep 1
-done
+sleep 5
 
 yarn cdklocal bootstrap
 yarn cdklocal deploy --all --require-approval never
