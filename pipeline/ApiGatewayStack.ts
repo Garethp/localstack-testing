6e97da94f9c482082a3fa8ef6f1fa4ee4bc90b88
Gareth Parker <gareth.parker@ros.gov.uk>
2025-12-11 14:08:57 +0000

    Remove extra code

--- pipeline/ApiGatewayStack.ts
+++ pipeline/ApiGatewayStack.ts
@@ -1,11 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib/core";
 import { Construct } from "constructs";
 import { RestApi } from "aws-cdk-lib/aws-apigateway";
-import { PublicHostedZone } from "aws-cdk-lib/aws-route53";
-import {
-  Certificate,
-  CertificateValidation,
-} from "aws-cdk-lib/aws-certificatemanager";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 
 export class ApiGatewayStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
@@ -13,13 +9,8 @@ export class ApiGatewayStack extends Stack {
 
     if (!!process.env.SECOND_DEPLOY) return;
 
-    // const hostedZone = new PublicHostedZone(this, "HostedZone", {
-    //   zoneName: "localhost.localstack.cloud",
-    // });
-
     const certificate = new Certificate(this, "Certificate", {
       domainName: "example.localhost.localstack.cloud",
-      // validation: CertificateValidation.fromDns(hostedZone),
     });
 
     const api = new RestApi(this, "RestApi", {
